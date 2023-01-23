02240700b50aba39d67caea65097c7db901a572d
Avijit Bhattacharjee <avijitbhattacharjee1995@gmail.com>
2023-01-24 01:00:14 +0530

    Download Book API Changes

--- operation/api.go
+++ operation/api.go
@@ -1,8 +1,17 @@
 package operation
 
 import (
+	"bytes"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
 	"github.com/avijit/config"
+	"github.com/avijit/pkg/handler"
 	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 var books []config.Book
@@ -41,6 +50,43 @@ func Crud(router *mux.Router) ([]config.Book, error) {
 	router.HandleFunc("/config/reserve/books/{id}", reserveBooks).Methods("GET")
 	router.HandleFunc("/config/release/books/{id}", releaseBooks).Methods("GET")
 	router.HandleFunc("/config/damage/books/{id}", damageBook).Methods("DELETE")
+	router.HandleFunc("/config/download/books", downloadBook).Methods("GET")
 
 	return books, nil
+}
+
+func downloadBook(w http.ResponseWriter, r *http.Request) {
+	response, err := http.Get("http://localhost:8080/config/books/author/Arijit")
+	if err != nil {
+		handler.RespondWithError(w, http.StatusInternalServerError, "Somehow host could not be reached.")
+		return
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	var books []config.Book
+	err = json.Unmarshal(data, &books)
+	if err != nil {
+		handler.RespondWithError(w, http.StatusInternalServerError, "Unable to unmarshal data.")
+		return
+	}
+	fmt.Println(books)
+	b := &bytes.Buffer{}
+	t := time.Now().Unix()
+	fileName := "address-book-" + strconv.FormatInt(t, 10) + ".csv"
+	writer := csv.NewWriter(b)
+	heading := []string{"id", "first_name", "last_name", "email_address", "phone_number"}
+	writer.Write(heading)
+	for _, eachEntry := range books {
+		var record []string
+		record = append(record, eachEntry.ID)
+		record = append(record, eachEntry.ISBN)
+		record = append(record, eachEntry.Author.LastName)
+		record = append(record, eachEntry.Author.FirstName)
+		writer.Write(record)
+	}
+	writer.Flush()
+	w.Header().Set("Content-Type", "text/csv") // setting the content type header to text/csv
+	w.Header().Set("Content-Disposition", "attachment;filename="+fileName)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b.Bytes())
+	return
 }
