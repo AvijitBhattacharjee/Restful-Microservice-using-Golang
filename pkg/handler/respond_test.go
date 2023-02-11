811e01f282ff9b50880540c67dbbf38fd95fe0b0
Avijit Bhattacharjee <avijitbhattacharjee1995@gmail.com>
2023-02-11 18:33:33 +0530

    Respond Unit Test Changes

--- pkg/handler/respond_test.go
+++ pkg/handler/respond_test.go
@@ -1,24 +1,51 @@
 package handler
 
 import (
-	"github.com/avijit/config"
 	"github.com/stretchr/testify/assert"
+	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
-func TestRespondWithError(t *testing.T) {
-	rr := httptest.NewRecorder()
-	RespondWithError(rr, 405, "error message")
-	assert.Equal(t, true, strings.Contains(rr.Result().Status, "Method Not Allowed"))
-	assert.Equal(t, 405, rr.Code)
-
-}
-
-func TestRespondWithSuccess(t *testing.T) {
-	rr := httptest.NewRecorder()
-	RespondWithJSON(rr, 200, "Hi")
-	assert.Equal(t, config.AppJsonContentType, rr.Header().Get(config.ContentType))
-	assert.Equal(t, 200, rr.Code)
+func TestRespond(t *testing.T) {
+	tests := []struct {
+		name           string
+		inputCode      int
+		inputPayload   string
+		wantErr        bool
+		expectedStatus int
+		expectedResult string
+	}{
+		{
+			name:           "Testing Response With Success",
+			inputCode:      200,
+			inputPayload:   "Success Case",
+			wantErr:        false,
+			expectedStatus: http.StatusOK,
+			expectedResult: "OK",
+		},
+		{
+			name:           "Testing Response With Error",
+			inputCode:      405,
+			inputPayload:   "Error Case",
+			wantErr:        true,
+			expectedStatus: http.StatusMethodNotAllowed,
+			expectedResult: "Method Not Allowed",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rr := httptest.NewRecorder()
+			if tt.wantErr == true {
+				RespondWithError(rr, 405, tt.inputPayload)
+				assert.Equal(t, true, strings.Contains(rr.Result().Status, tt.expectedResult))
+				assert.Equal(t, tt.expectedStatus, rr.Code)
+			} else {
+				RespondWithJSON(rr, 200, tt.inputPayload)
+				assert.Equal(t, true, strings.Contains(rr.Result().Status, tt.expectedResult))
+				assert.Equal(t, tt.expectedStatus, rr.Code)
+			}
+		})
+	}
 }
